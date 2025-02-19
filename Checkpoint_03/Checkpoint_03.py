bba30496327605a410622fe2085c68c61d1d895d
arrese-a <ainhoa.arrese@gmail.com>
2025-02-19 10:54:49 +0100

    Add Checkpoint_03

---
+++ Checkpoint_03/Checkpoint_03.py
@@ -0,0 +1,36 @@
+# Exercise 1
+my_str = 'This is Checkpoint 3'
+my_num = 5
+my_list = ['one', 'two', 'three']
+my_bool = True
+
+# Exercise 2 
+new_str = my_str[:3]
+
+# Exerecise 3
+first_list = my_list[0]
+
+# Exercise 4
+new_num = my_num + 10
+
+# Exercise 5
+last_list = my_list[-1]
+
+# Exercise 6 
+names = 'harry,alex,susie,jared,gail,conner'
+names = names.split(',')
+
+# Exercise 7
+sentence_first = my_str[:4].upper()
+compound_sentence = sentence_first + my_str [4:]
+
+# Exercise 8
+str_inter = f'The value of the number variable is {my_num}'
+
+# Exercise 9
+print('Hello world')
+
+# Extra exercise 
+sentence = 'Hola, este es el ejercicio extra del ticket'
+query = sentence.find('Hola')
+new_sentence = sentence.replace('Hola', 'adiós')
